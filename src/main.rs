337d8b8302d01cf18357ed9020c6168d56a123bc
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2021-09-28 13:13:26 +0900

    VGA text buffer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![no_std]
 #![no_main]
 
+#[allow(dead_code)]
 static HELLO: &[u8] = b"Hello World!";
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -12,16 +14,27 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    // let vga_buffer = 0xb8000 as *mut u8;
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     // stringをVGAのcolor byteに変換してる
+    //     // メモリのポインタに直接アクセスしてるのでunsafeを使ってるが多用すべきでない
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        // stringをVGAのcolor byteに変換してる
-        // メモリのポインタに直接アクセスしてるのでunsafeを使ってるが多用すべきでない
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // use core::fmt::Write;
+    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    // write!(
+    //     vga_buffer::WRITER.lock(),
+    //     ", some numbers: {} {}",
+    //     42,
+    //     1.337
+    // )
+    // .unwrap();
+
+    println!("Hello World{}", "!");
 
     loop {}
 }

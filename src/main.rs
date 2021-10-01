e4b87f476251eba7958cc89afe917a0228a41f76
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2021-10-01 14:25:27 +0900

    Double Fault Exceptions

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,19 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::init();
 
-    x86_64::instructions::interrupts::int3();
+    // breakpoint
+    // x86_64::instructions::interrupts::int3();
+
+    // Page Fault
+    // unsafe {
+    //     *(0xdeadbeef as *mut u64) = 42;
+    // };
+
+    // Stack Overflow
+    // fn stack_overflow() {
+    //     stack_overflow();
+    // }
+    // stack_overflow();
 
     #[cfg(test)]
     test_main();
